4d7cbb942706cc9f48165f44807326d568a3bc03
Ashwin02 <ashwinabraham2001@gmail.com>
2025-06-09 16:51:52 +0530

    Added CommandInvoker

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+public class CommandInvoker
+{
+    // A stack to keep track of executed commands.
+    private Stack<ICommand> commandRegistry = new Stack<ICommand>();
+
+
+    public void ProcessCommand(ICommand commandToProcess)//Process a command, which involves both executing it and registering it.
+    {
+        ExecuteCommand(commandToProcess);
+        RegisterCommand(commandToProcess);
+    }
+
+    public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();// Execute a command, invoking its associated action.
+
+    public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);// Register a command by adding it to the command registry stack.
+}
