df43c38b7c032b2f58b457f6b8397c64e80b2acc
Ashwin02 <ashwinabraham2001@gmail.com>
2025-06-09 16:07:27 +0530

    Added Interface and abstract class for Abstract Command Setpu

---
+++ Assets/Scripts/Command/Abstract Command/UnitCommand.cs
@@ -0,0 +1,18 @@
+using Command.Player;
+
+public abstract class UnitCommand : ICommand
+{
+    // Fields to store information related to the command.
+    public int ActorUnitID;
+    public int TargetUnitID;
+    public int ActorPlayerID;
+    public int TargetPlayerID;
+
+    // References to the actor and target units, accessible by subclasses.
+    protected UnitController actorUnit;
+    protected UnitController targetUnit;
+
+    public abstract void Execute(); //Abstract method to execute the unit command.Must be implemented by concrete subclasses.
+
+    public abstract bool WillHitTarget();//Abstract method to determine whether the command will successfully hit its target.
+}
