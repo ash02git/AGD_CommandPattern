0c2b42a076329bf03d2cb36a46d9c0650855b63e
Ashwin02 <ashwinabraham2001@gmail.com>
2025-06-11 15:50:18 +0530

    Implemented replay

--- Assets/Scripts/Command/CommandInvoker.cs
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -7,6 +7,15 @@ namespace Command.Commands
         // A stack to keep track of executed commands.
         private Stack<ICommand> commandRegistry = new Stack<ICommand>();
 
+        public CommandInvoker() => SubscribeToEvents();
+
+        private void SubscribeToEvents() => GameService.Instance.EventService.OnReplayButtonClicked.AddListener(SetReplayStack);
+
+        public void SetReplayStack()
+        {
+            GameService.Instance.ReplayService.SetCommandStack(commandRegistry);
+            commandRegistry.Clear();
+        }
 
         public void ProcessCommand(ICommand commandToProcess)//Process a command, which involves both executing it and registering it.
         {
